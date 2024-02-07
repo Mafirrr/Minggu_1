aa3b252240e19f14c7d9af20990cabf129002568
Mafirrr <84912073+Mafirrr@users.noreply.github.com>
2024-02-07 19:21:53 +0700

    Initial commit

---
+++ src/minggu_1/Minggu_1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minggu_1;
+
+/**
+ *
+ * @author Mafir
+ */
+public class Minggu_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mafira Aurelia Salsabila Firdaus");
+        System.out.print("NIM E41232457");
+        System.out.println(" " + "Golongan A");
+        System.out.println("Teknik Informatika Kampus Bondowoso");
+    }
+    
+}
